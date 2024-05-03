e9c442aa14e68cf80b9dc8d5c66f67ceac5b3af7
Tilak Singh <tsingh99991@gmail.com>
2024-05-03 19:04:48 +0530

    account db added

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 //url handy
-const url = "mongodb+srv://tilak001:tilak001@cluster0.1dhtbga.mongodb.net/paytmApp";
+const url =
+  "mongodb+srv://tilak001:tilak001@cluster0.1dhtbga.mongodb.net/paytmApp";
 
 mongoose.connect(url);
 const userTable = mongoose.Schema({
@@ -23,7 +24,21 @@ const userTable = mongoose.Schema({
   },
 });
 
+//accout data reference
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",//reference
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 const User = mongoose.model("User", userTable);
+const Account = mongoose.model("Account",accountSchema)
 
 module.exports = {
   User,
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,6 +12,33 @@ const updateSchema = zod.object({
   lastName: zod.string().optional(),
 });
 
+router.get("/search", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+  res.status(200).json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
 router.put("/profile", authHeader, async (req, res) => {
   const { success } = updateSchema.safeParse(req.body);
   if (!success) {
